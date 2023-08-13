aa4ea4a4e61432585e2955f685354525fee80bad
katia <@Keyt92>
2023-08-13 18:20:01 +0200

    work

--- app/services/get_formatted_output.py
+++ app/services/get_formatted_output.py
@@ -1,2 +1,9 @@
 def get_formatted_output(data) -> str:
-    pass
+    output = ""
+    for group_name, group_data in data.items():
+        quantity = group_data["quantity"]
+        members = ", ".join(group_data["members"])
+        output += f"Group: {group_name}, Quantity: {quantity}, Members: {members}\n"
+
+    return output
+
